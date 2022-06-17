c48176756b2131dcaf5961aa9e9dc833b43f42c6
tcskill <tskill@us.ibm.com>
2022-06-17 14:36:03 -0500

    removes namespace ref

--- test/stages/stage2-mymodule.tf
+++ test/stages/stage2-mymodule.tf
@@ -4,7 +4,6 @@ module "gitops_module" {
   gitops_config = module.gitops.gitops_config
   git_credentials = module.gitops.git_credentials
   server_name = module.gitops.server_name
-  namespace = module.gitops_namespace.name
   kubeseal_cert = module.gitops.sealed_secrets_cert
 
   instanceid = module.iot.instname
