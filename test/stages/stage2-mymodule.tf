d1a1e5aa3e9fdebaeecd5ab64cd6c67e50b5eb58
tcskill <tskill@us.ibm.com>
2022-06-18 09:15:17 -0500

    fixes template increases sleeper

--- test/stages/stage2-mymodule.tf
+++ test/stages/stage2-mymodule.tf
@@ -1,14 +1,15 @@
-resource "time_sleep" "wait_10_mins" {
+resource "time_sleep" "wait_20_mins_for_iot" {
   depends_on = [ module.iot ]
 
   // we need to do some throtteling due to rate limits being hit
   // the iot stack would most likely be done in a separate layer as well
-  create_duration = "10m"
+  // and prevents needing a separate cluster just for iot prereq
+  create_duration = "20m"
 }
 
 module "gitops_module" {
   source = "./module"
-  depends_on = [ time_sleep.wait_10_mins ]
+  depends_on = [ time_sleep.wait_20_mins_for_iot ]
 
   gitops_config = module.gitops.gitops_config
   git_credentials = module.gitops.git_credentials
